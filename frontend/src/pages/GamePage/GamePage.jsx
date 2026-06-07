3a35733ef512f8cde12a984bc0cbac2659df177e
MarcosGarciaC <marcos.garciacastillo459@gmail.com>
2026-06-06 21:12:42 -0600

    Game page

--- frontend/src/pages/GamePage/GamePage.jsx
+++ frontend/src/pages/GamePage/GamePage.jsx
@@ -84,9 +84,9 @@ const GamePage = () => {
 
             <div className="header-desc__right">
               <button className='btn-add_fav'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-heart-fill" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"/>
-</svg> AGREGAR A FAV</button>
-              <button>ESCRIBE UNA RESEÑA</button>
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+              </svg> AGREGAR A FAV</button>
+              <button className='btn-wrt__opinion'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil-square" viewBox="0 0 16 16"> <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" /> <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" /> </svg> ESCRIBE UNA RESEÑA</button>
             </div>
           </div>
         </div>
@@ -103,7 +103,7 @@ const GamePage = () => {
             </div>
           </div>
           <div className='description-side'>
-           <h3 className="veredict-title">DESCRIPCION</h3>
+            <h3 className="veredict-title">DESCRIPCION</h3>
             <p className="veredict-text">{game.description}</p>
           </div>
         </div>
